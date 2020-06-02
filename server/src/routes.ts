f573e067132ee0dd51298f550c244231e0cc6c08
CaueFurui <cauefurui@gmail.com>
2020-06-02 19:59:15 -0300

    added item listing

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-    return res.json({ message: "Hello World!" })
+routes.get("/items", async (req, res) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        };
+    }) 
+
+    return res.json(serializedItems);
 });
+
+routes.post
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 // Rota: Endereço completo da Requisição
 // Recurso: Qual entidade estamos acessando do sistema
