b4c43d8d79ee6551063de9800fe9ebd1485817fe
CaueFurui <cauefurui@gmail.com>
2020-06-02 18:53:57 -0300

    created database

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -5,7 +5,9 @@ const connection = knex({
     client: "sqlite3",
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite')
-    }
+    },
+    useNullAsDefault: true,
+    
 });
 
 export default connection;
