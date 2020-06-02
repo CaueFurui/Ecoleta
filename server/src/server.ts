824e980a5fbaa75865489bdb3274fc5e94124bec
CaueFurui <cauefurui@gmail.com>
2020-06-02 17:01:26 -0300

    generated connection with the database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,22 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get("/users", (req, res) => {
-    console.log("Listagem de usuários");
+app.use(express.json());
+app.use(routes);
 
+// Rota: Endereço completo da Requisição
+// Recurso: Qual entidade estamos acessando do sistema
 
-    res.json([
-        'Diego 5000',
-        'Cleiton',
-        'Robson'
-    ]);
-});
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informações
+
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson'
+];
 
 app.listen(3333);
