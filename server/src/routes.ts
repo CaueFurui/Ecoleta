80a5eafb42c8953a130fa403bee86a4cca2738c7
CaueFurui <cauefurui@gmail.com>
2020-06-02 22:24:02 -0300

    added post points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,19 +3,57 @@ import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get("/items", async (req, res) => {
+routes.get('/items', async (req, res) => {
     const items = await knex('items').select('*');
 
     const serializedItems = items.map(item => {
         return {
+            id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`
         };
-    }) 
+    })
 
     return res.json(serializedItems);
 });
 
-routes.post
+routes.post('/points', async (req, res) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items,
+    } = req.body
+
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    });
+
+    const point_id = insertedIds[0];
+
+    const pointItems = items.map((item_id: number) =>{
+        return {
+            item_id,
+            point_id,
+        }
+    })
+
+    await trx('point_items').insert(pointItems)
+
+    return res.json({ success: 'true' });
+})
 
 export default routes;
