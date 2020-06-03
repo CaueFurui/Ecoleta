85df78d00ae7c886887f34d06c200da9f2b6c6ff
CaueFurui <cauefurui@gmail.com>
2020-06-03 00:12:35 -0300

    added cors and static image for points

--- server/src/controllers/PointsControllers.ts
+++ server/src/controllers/PointsControllers.ts
@@ -37,7 +37,7 @@ class PointsControllers {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1481761289552-381112059e05?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
